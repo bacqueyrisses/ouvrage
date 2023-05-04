696a5adda64eed53066a2be8803659da42b08c2c
Enzo Bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2023-05-04 12:41:40 +0200

    Add CRON config

--- app/components/WordsList.tsx
+++ app/components/WordsList.tsx
@@ -3,13 +3,7 @@ import { sql } from "@vercel/postgres";
 export default async function WordsList() {
   const { rows } = await sql`SELECT * FROM "Word";`;
   const index = Math.round(Math.random() * (rows.length - 1));
-  console.log(index);
 
   // @ts-ignore
-  return (
-    <>
-      <div>{rows[index].title}</div>
-      <div>{rows[index].description}</div>
-    </>
-  );
+  return <div>{rows[index]?.title}</div>;
 }
