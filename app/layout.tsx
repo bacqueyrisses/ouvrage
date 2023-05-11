244cef82545505d60fc8067aa1fe46fcb46280e8
Enzo Bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2023-05-11 17:48:29 +0200

    Change revalidate timing

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ export const metadata = {
   description: `Un mot quotidien provenant d'une collection de mots mise à jour à travers les différentes rencontres de la vie`,
 };
 
-export const revalidate = 10;
+export const revalidate = 86400;
 
 export default async function RootLayout({
   children,
