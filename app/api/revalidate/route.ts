984a449f2eceb92a42e8cb89a15991acd0fbe454
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2023-07-26 14:18:15 +0200

    Update revalidation method

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server'
 import { revalidatePath } from 'next/cache'
 
 export async function GET(request: NextRequest) {
-    await fetch('https://www.ouvrage.dev/')
+    await fetch('https://www.ouvrage.dev/', {method: "GET"})
     revalidatePath("/")
     return NextResponse.json({ revalidated: true, now: Date.now() })
 }
