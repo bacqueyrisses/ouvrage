0e942e54470f681338ced40c5306caa7c128ca17
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2023-08-01 18:25:57 +0200

    Update logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,57 +1,59 @@
 import Image from "next/image";
-import {Button} from "@/components/ui/button";
-import {User} from "lucide-react";
-import Link from "next/link";
 import SearchControl from "@/components/ui/custom/SearchControl";
 import ModeSwitcher from "@/components/ModeSwitcher";
+import logo from "@/public/logo.webp";
+import logoDark from "@/public/logo-dark.webp";
 
 interface HeaderI {
-    words: string[]
-    handleSearch: (word: string) => void
-    handleHomepage: () => void
-
+  words: string[];
+  handleSearch: (word: string) => void;
+  handleHomepage: () => void;
 }
-export default function Header({words, handleSearch, handleHomepage}: HeaderI) {
-    return (
-        <header className="w-full">
-            <div className="container flex h-20 items-center">
-                <div className="mr-4 flex">
-                    <button className="mr-6 flex items-center space-x-2" onClick={handleHomepage}>
-                        <Image
-                            src="./book.svg"
-                            alt="Next.js Logo"
-                            className={"inline dark:hidden"}
-                            width={25}
-                            height={25}
-                            priority
-                        />
-                        <Image
-                            src="./book-dark.svg"
-                            className={"hidden dark:inline"}
-                            alt="Next.js Logo"
-                            width={25}
-                            height={25}
-                            priority
-                        />
-                        <span className="hidden font-bold sm:inline-block">Ouvrage</span>
-                    </button>
-                    <div
-                        className={
-                            "hidden rounded-2xl bg-secondary px-4 py-1.5 text-sm font-medium text-secondary-foreground hover:bg-secondary/80 md:block"
-                        }
-                    >
-                        Le lexique des jolis mots
-                    </div>
-                </div>
-                <div className="flex flex-1 items-center justify-between space-x-2 sm:space-x-4 md:justify-end">
-                    <SearchControl words={words} handleSearch={handleSearch}/>
-                    {/*<Button variant="secondary" className={"h-9"}>*/}
-                    {/*  <span className={"hidden sm:inline-block"}>Connexion</span>*/}
-                    {/*  <User size={19} className={"sm:hidden"} />*/}
-                    {/*</Button>*/}
-                    <ModeSwitcher/>
-                </div>
-            </div>
-        </header>
-    );
+export default function Header({
+  words,
+  handleSearch,
+  handleHomepage,
+}: HeaderI) {
+  return (
+    <header className="w-full">
+      <div className="container flex h-20 items-center">
+        <div className="flex items-center gap-2">
+          <button className="flex items-center" onClick={handleHomepage}>
+            <Image
+              src={logo}
+              alt="Ouvrage light Logo"
+              className={"inline dark:hidden"}
+              width={52}
+              height={52}
+              priority
+            />
+            <Image
+              src={logoDark}
+              className={"hidden dark:inline"}
+              alt="Ouvrage dark Logo"
+              width={52}
+              height={52}
+              priority
+            />
+            {/*<span className="hidden font-bold sm:inline-block">Ouvrage</span>*/}
+          </button>
+          <div
+            className={
+              "inline hidden rounded-2xl bg-secondary px-4 py-1.5 text-sm font-medium text-secondary-foreground hover:bg-secondary/80 md:block"
+            }
+          >
+            Le lexique des jolis mots
+          </div>
+        </div>
+        <div className="flex flex-1 items-center justify-between space-x-2 sm:space-x-4 md:justify-end">
+          <SearchControl words={words} handleSearch={handleSearch} />
+          {/*<Button variant="secondary" className={"h-9"}>*/}
+          {/*  <span className={"hidden sm:inline-block"}>Connexion</span>*/}
+          {/*  <User size={19} className={"sm:hidden"} />*/}
+          {/*</Button>*/}
+          <ModeSwitcher />
+        </div>
+      </div>
+    </header>
+  );
 }
