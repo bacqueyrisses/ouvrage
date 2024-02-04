25e21a636aaf5885c3a5139ab14475c37187b053
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2024-02-04 15:57:24 +0900

    Update portfolio link in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <div className={"text-center text-sm"}>
-      <Link href={"https://www.bacqueyrisses.dev/"} target={"_blank"}>
+      <Link href={"https://www.enzo.codes"} target={"_blank"}>
         ✦ Made with love by Enzo ✦
       </Link>
     </div>
