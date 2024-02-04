f7e2c6225b65944b7b46ad3a8fea478e48e5859a
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2024-02-04 18:11:43 +0900

    Update url in CRON with env variable

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,8 +5,4 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const baseURL = process.env.VERCEL_URL
-  ? `https://www.ouvrage.dev`
-  : `http://localhost:${process.env.PORT ?? 3000}`;
-
-export const getCurrentDay = new Date().toLocaleDateString()
+export const getCurrentDay = new Date().toLocaleDateString();
