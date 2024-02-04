3b010c9853b39c1efd96a4cca86faf3d398986bc
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2024-02-04 18:43:38 +0900

    Update CRON route

--- app/api/cron/revalidate/route.ts
+++ app/api/cron/revalidate/route.ts
@@ -1,9 +1,9 @@
 import { NextResponse } from "next/server";
 import axios from "axios";
 export async function GET() {
-  if (!process.env.VERCEL_URL) throw new Error("App URL not defined.");
+  if (!process.env.APP_URL) throw new Error("App URL not defined.");
   try {
-    await axios.get(process.env.VERCEL_URL);
+    await axios.get(process.env.APP_URL);
   } catch (error) {
     console.error(error);
     throw new Error("Couldn't fetch url.");
